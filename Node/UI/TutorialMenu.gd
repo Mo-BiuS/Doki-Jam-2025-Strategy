32dd1472156cb38a6f727add70b578d38f1ec9c9
Mo-BiuS <lodland95@gmail.com>
2025-08-15 16:48:06 +0200

    tutorial : controls

--- Node/UI/TutorialMenu.gd
+++ Node/UI/TutorialMenu.gd
@@ -2,10 +2,24 @@ class_name TutorialMenu extends CanvasLayer
 
 signal toMainMenu
 
+@export var tutoContainer:Control
+var pointer = 0
+
+func _ready() -> void:
+	refreshTuto()
 
 func _on_prev_tutoriel_pressed() -> void:
-	pass # Replace with function body.
+	if(pointer+1 < tutoContainer.get_children().size()):pointer+=1
+	else:pointer = 0
+	refreshTuto()
 func _on_next_tutoriel_pressed() -> void:
-	pass # Replace with function body.
+	if(pointer-1 >= 0):pointer-=1
+	else:pointer = tutoContainer.get_children().size()-1
+	refreshTuto()
 func _on_to_main_menu_pressed() -> void:
 	toMainMenu.emit()
+
+func refreshTuto():
+	for j in tutoContainer.get_children():
+		if(j.name == "Tuto"+str(pointer)):j.show()
+		else:j.hide()
